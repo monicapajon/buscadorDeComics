a945c79d42137322c3f8d387bee4e2fa99ac3ee5
Monica Pajon <moniestudio161@gmail.com>
2022-11-06 16:30:51 -0300

    pagination

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ const resultados = document.querySelector(".results");
 const resultadosPorPagina = 20;
 let paginaActual = 0;
 let cantidadDeResultados = 0;
+
+// =>PAGINACION
+const primeraPagina = document.querySelector(".button__first-left");
+const ultimaPagina = document.querySelector(".button__last");
+const proximaPagina = document.querySelector(".button__right");
+const anteriorPagina = document.querySelector(".button__left");
